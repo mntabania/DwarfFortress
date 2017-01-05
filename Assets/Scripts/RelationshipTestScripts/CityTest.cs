7de7f4a6a3fde06546320d436e78b20745c07116
Mykel Abania <mntabania@gmail.com>
2017-01-05 19:38:48 +0800

    +City and Kingdom Test Scripts

---
+++ Assets/Scripts/RelationshipTestScripts/CityTest.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CityTest{
+
+	public int id;
+	public string cityName;
+	public BIOMES biomeType;
+	public Mayor cityMayor;
+	public int cityLevel;
+	public int numOfRoads;
+	public int population;
+	public int foodCount;
+	public int lumberCount;
+	public int rockCount;
+	public int manaStoneCount;
+	public int tradeGoodsCount;
+	public int mayorLikeRating;
+	public List<Citizen> citizens;
+	public List<CityTile> connectedCities;
+	public Religion cityReligion;
+	public Culture cityCulture;
+	public KingdomTile kingdomTile;
+	public HexTile hexTile;
+
+	public CityTest(HexTile hexTile, BIOMES biomeType){
+		this.id = 0;
+		this.cityName = hexTile.name;
+		this.biomeType = biomeType;
+		this.cityMayor = new Mayor();
+		this.cityLevel = 1;
+		this.numOfRoads = 0;
+		this.population = 0;
+		this.foodCount = 0;
+		this.lumberCount = 0;
+		this.rockCount = 0;
+		this.manaStoneCount = 0;
+		this.tradeGoodsCount = 0;
+		this.mayorLikeRating = 0;
+		this.citizens = new List<Citizen>();
+		this.connectedCities = new List<CityTile>();
+		this.cityReligion = new Religion();
+		this.cityCulture = new Culture();
+		this.kingdomTile = new KingdomTile();
+		this.hexTile = hexTile;
+	}
+
+//	public HexTile hexTile;
+//	public BIOMES biomeType;
+//	public CITY_TYPE cityType;
+//	public int cityLevel;
+//	public int richnessLevel;
+//	public int numOfRoads;
+//	public int population;
+//	public int garrison;
+//	public int gold;
+//	public List<CityTile> connectedCities;
+//	public KingdomTile kingdomTile;
+//	public Faction faction;
+}
---
+++ Assets/Scripts/RelationshipTestScripts/KingdomTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KingdomTest{
+
+	public int id;
+	public string kingdomName;
+	public King king;
+	public List<CityTest> cities;
+	public RACE kingdomRace;
+	public bool isDead;
+	public List<Relationship> relationshipKingdoms;
+
+	public KingdomTest(){
+		this.id = 0;
+		this.kingdomName = "KINGDOM";
+		this.king = new King();
+		this.cities = new List<CityTest>();
+		this.kingdomRace = RACE.HUMANS;
+		this.isDead = false;
+		this.relationshipKingdoms = new List<Relationship>();
+	}
+
+}
